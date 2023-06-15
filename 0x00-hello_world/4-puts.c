b859c72e4ebe7882825dcaa2e29e7904410cf10c
linetgitonga <linetgitonga55@gmail.com>
2023-06-15 14:53:18 -0700

    c_low-level language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/*
+ * File: 4-puts.c
+ * return:print a line using puts
+ */
+int main(void){
+
 puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,16 @@
+/*
+ * File: 5-printf.c
+ * /
+ 
 #include <stdio.h>
-printf("with proper grammar, but the outcome is a piece of art,")
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main (void){
+printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
