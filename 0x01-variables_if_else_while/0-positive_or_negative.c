6f39b35ba55d0afe5bffab18875301067a324825
linetgitonga <linetgitonga55@gmail.com>
2023-06-16 01:45:39 -0700

    Positive,zero and negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/**
+ *
+ *main "print if a number is positive
+ *
+ *
+ */
+int main(void){
+int a;
+if( a>o){
+	printf("is positive\n");
+}
+else if (a=o){
+	printf("is zero\n");
+}
+else{
+	printf("is negative\n");
+}
+return(0);
+}
