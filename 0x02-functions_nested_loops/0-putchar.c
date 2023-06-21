9de24749eddcc1fa4ce19a12a523134ceff9a9df
linetgitonga <linetgitonga55@gmail.com>
2023-06-20 17:51:03 -0700

    loops task 7 to 11

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,11 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
 int main(void)
 {
-	return (write(1, &c, 1));
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
