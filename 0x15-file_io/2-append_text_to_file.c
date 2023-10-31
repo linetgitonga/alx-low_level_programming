c18f523b38455ec8c8e1415dd774dac010fc179d
linetgitonga <linetgitonga55@gmail.com>
2023-10-30 17:47:40 -0700

    updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,104 +1,39 @@
 /*
- * Task 3: copy
+ * Task 2: Append text to end of file
  * Author: Linet Gitonga
  */
 
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-char *create_buffer(char *file);
-void close_file(int f);
 
 /**
- * create_buffer - Allocates kb for a buffer.
- * @file: filename buffer is storing chars for.
+ * append_text_to_file -F appends text at the end of a file.
+ * @filename: The name of a file
+ * @text_content: Content of text
  *
- * Return: buffer pointer
+ * Return: integer
  */
-char *create_buffer(char *file)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	char *buf;
+	int fdi, stat = -1;
+	int leng, a;
 
-	buf = malloc(sizeof(char) * 1024);
+	if (filename == NULL)
+		return (stat);
 
-	if (buf == NULL)
+	fdi = open(filename, O_WRONLY | O_APPEND);
+	if (fdi == -1)
+		return (stat);
+	stat = 1;
+
+	if (text_content != NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
+		for (a = 0, leng = 0; text_content[a] != '\0'; a++)
+			leng++;
+		stat = write(fdi, text_content, leng);
+		if (stat != -1)
+			stat = 1;
 	}
-
-	return (buf);
-}
-
-/**
- * close_file - Close the file descriptors.
- * @f:dedscriptor to be closed.
- */
-void close_file(int f)
-{
-	int j;
-
-	j = close(f);
-
-	if (j == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close f %d\n", f);
-		exit(100);
-	}
-}
-
-/**
- * main - Function copies the contents of a file .
- * @argc: number of arguments in the program.
- * @argv: pointers arrays to the arguments.
- *
- * Return: 0 on success.
- */
-int main(int argc, char *argv[])
-{
-	int from, to, rd, wr;
-	char *buf;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	buf = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	rd = read(from, buf, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (from == -1 || rd == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read  file %s\n", argv[1]);
-			free(buf);
-			exit(98);
-		}
-
-		wr = write(to, buf, rd);
-		if (to == -1 || wr == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buf);
-			exit(99);
-		}
-
-		rd = read(from, buf, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (rd > 0);
-
-	free(buf);
-	close_file(from);
-	close_file(to);
-
-	return (0);
+	close(fdi);
+	return (stat);
 }
 
